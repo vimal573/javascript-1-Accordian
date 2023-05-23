fd695029989f76d99b00022497a04337dc676d40
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-05-23 23:06:08 +0530

    add

--- faq.js
+++ faq.js
@@ -1,28 +1,28 @@
 const faqData = [
   {
     id: 1,
-    question: "Who are we?",
+    question: 'Who are we?',
     answer:
-      "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
+      'We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.',
   },
   {
     id: 2,
-    question: "What do we do?",
+    question: 'What do we do?',
     answer:
-      "Building learning communities with Our Affordable & Competent Courses",
+      'Building learning communities with Our Affordable & Competent Courses',
   },
   {
     id: 3,
-    question: "Are the community classes boring?",
-    answer: "No, not at all!!!",
+    question: 'Are the community classes boring?',
+    answer: 'No, not at all!!!',
   },
 ];
 
-const accordianBody = document.querySelector(".accordian_body");
+const accordianBody = document.querySelector('.accordian_body');
 const faqs = [];
 
 function createFaq() {
-  faqData.forEach((el) => {
+  faqData.forEach(el => {
     const html = `
     <div class="faq">
       <div class="faq_header">
@@ -39,22 +39,22 @@ function createFaq() {
 function showFaq() {
   createFaq();
   const markup = faqs.join();
-  accordianBody.insertAdjacentHTML("beforeend", markup);
+  accordianBody.insertAdjacentHTML('beforeend', markup);
   // accordianBody.innerHTML = markup;
 }
 showFaq();
 
 function btnStatusUpdate() {
-  document.querySelectorAll(".show_btn").forEach((el) => {
-    el.addEventListener("click", function (e) {
-      const text = document.querySelectorAll(".text");
+  document.querySelectorAll('.show_btn').forEach(el => {
+    el.addEventListener('click', function (e) {
+      const text = document.querySelectorAll('.text');
 
-      text.forEach((el) => {
+      text.forEach(el => {
         // Showing hidden answer
         if (e.target.dataset.id === el.dataset.id) {
-          el.classList.toggle("hidden");
+          el.classList.toggle('hidden');
         } else {
-          el.classList.add("hidden");
+          el.classList.add('hidden');
         }
       });
     });
